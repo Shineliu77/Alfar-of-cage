03a25d260eca0fd6a2f1b7769216a93ee5e1a9ca
Xueli1106 <144639254+Xueli1106@users.noreply.github.com>
2024-03-20 12:14:28 +0800

    新進度0320
    ＞新增動畫狀態
    ＞調整鏡頭

---
+++ Assets/Scripts/Player/PlayerAnimetion.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimetion : MonoBehaviour
+{
+    private Animator anim;
+    public Rigidbody2D RB;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        RB = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void Update()
+    {
+        SetAnimetion();
+    }
+
+    private void SetAnimetion()
+    {
+        anim.SetFloat("velocityx", Mathf.Abs(RB.velocity.x));
+    }
+
+}
